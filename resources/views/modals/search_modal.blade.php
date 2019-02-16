4924c3d74f6d163fbb32af2d0f193d83a18dcc92
Unknown <talles_of_destiny@yahoo.com.sg>
2019-02-17 01:32:05 +0900

    Fixed home page search modal and upload modal

--- resources/views/modals/search_modal.blade.php
+++ resources/views/modals/search_modal.blade.php
@@ -1,24 +1,26 @@
 <div id="search-modal" class="modal modal-danger fade" tabindex="-1" role="dialog" aria-labelledby="custom-width-modalLabel" aria-hidden="true" style="display: none;">
     <div class="modal-dialog">
         <div class="modal-content">
-            <form  method="POST" id="reset_confirmation">
-            <div class="modal-header">
-              <h4 class="modal-title text-center" id="custom-width-modalLabel">Search Food</h4>
-              <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button> 
+        	{!! Form::open(['url' => '', 'id' => 'reset_confirmation']) !!}
+        	<div class="modal-header">
+        		<h4 class="modal-title text-center" id="custom-width-modalLabel">Search Food</h4>
+        		<button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button> 
             </div>
             <div class="modal-body">
-              <label>Allergens</label>
-              <select class="form-control">
-                <option>Default select</option>
-              </select>
-              <label>Type the name of food that you want to know about</label>
-              <input type="text" class="form-control" id="food_title" placeholder="calamary">
+            	<div class="form-group">
+	            	<label>Allergens</label>
+	            	{!! Form::select('size', [], null, ['placeholder' => 'Select a Allergen', 'class' => 'form-control']) !!}
+            	</div>
+            	<div class="form-group">
+	            	<label>Type the name of food that you want to know about</label>
+	            	{!! Form::text('username', null, ['class' => 'form-control', 'placeholder' => 'calamary', 'id' => 'food_title']) !!}
+            	</div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary waves-effect remove-data-from-delete-form" data-dismiss="modal">Cancel</button>
-                <button type="submit" class="btn btn-warning waves-effect remove-data-from-delete-form">Search</button>
+            	<button type="button" class="btn btn-danger waves-effect remove-data-from-delete-form" data-dismiss="modal">Cancel</button>
+            	<button type="submit" class="btn btn-primary waves-effect remove-data-from-delete-form">Search</button>
             </div>
-            </form>
+        	{!! Form::close() !!}
         </div>
     </div>
 </div>
--- resources/views/modals/upload_modal.blade.php
+++ resources/views/modals/upload_modal.blade.php
@@ -1,30 +1,38 @@
 <div id="upload-picture-modal" class="modal modal-danger fade" tabindex="-1" role="dialog" aria-labelledby="custom-width-modalLabel" aria-hidden="true" style="display: none;">
     <div class="modal-dialog">
         <div class="modal-content">
-            <form  method="POST" id="reset_confirmation">
+            {!! Form::open(['url' => '', 'id' => 'reset_confirmation']) !!}
             <div class="modal-header">
                 <h4 class="modal-title text-center" id="custom-width-modalLabel">Upload Image</h4>
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">×</button> 
             </div>
             <div class="modal-body">
-              <label>Choose image to upload</label>
-              <input type="file" class="form-control-fileupload col-xl-7 col-12" name="file" id="file" required="required" onchange="loadFile(event)">
-              <div class="item form-group">
-              <label class="control-label col-md-3 col-sm-3 col-xs-12"><span class="required"></label>
-              <img id="image" width="250px" height="200px"></img>
-              </div>
+                <div class="input-group">
+                    <div class="custom-file">
+                        <label class="custom-file-label">Choose file: 
+                            <span id="file-name"></span>
+                        </label>
+                        {!! Form::file('file', ['class' => 'custom-file-input', 'onchange' => 'loadFile(event)']) !!}
+                    </div>
+                </div>
+                <div class="item form-group">
+                    <img id="image" height="200px"></img>
+                </div>
             </div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-primary waves-effect remove-data-from-delete-form" data-dismiss="modal">Cancel</button>
-                <button type="submit" class="btn btn-danger waves-effect remove-data-from-delete-form">Search</button>
+                <button type="button" class="btn btn-danger waves-effect remove-data-from-delete-form" data-dismiss="modal">Cancel</button>
+                <button type="submit" class="btn btn-primary waves-effect remove-data-from-delete-form">Search</button>
             </div>
-            </form>
+            {!! Form::close() !!}
         </div>
     </div>
 </div>
 <script type="text/javascript">
   var loadFile = function(event) {
     var image = document.getElementById('image');
-    image.src = URL.createObjectURL(event.target.files[0]);
+    var file = event.target.files[0];
+
+    image.src = URL.createObjectURL(file);
+    document.getElementById('file-name').innerHTML = file.name;
   };
 </script>
